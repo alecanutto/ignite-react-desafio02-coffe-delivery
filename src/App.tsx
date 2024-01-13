82f08f2d5a2db7533aac19b986358f6768c918eb
Alessandra Canuto <ale.canutto@gmail.com>
2024-01-13 16:25:05 -0300

    feat: add routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { ThemeProvider } from 'styled-components'
+import { Outlet } from 'react-router-dom'
+
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
-function App() {
+export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      Coffe Delivery
+      <Outlet />
     </ThemeProvider>
   )
 }
-
-export default App
