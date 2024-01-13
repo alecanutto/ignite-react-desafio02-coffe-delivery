ca92ddc71c9913601f4a0f60aac93857c54db5b5
Alessandra Canuto <ale.canutto@gmail.com>
2024-01-13 17:53:11 -0300

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Outlet } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { CartContextProvider } from './contexts/CartContextProvider'
+import { Header } from './components/Header'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <CartContextProvider>
+        <Header />
         <Outlet />
       </CartContextProvider>
     </ThemeProvider>
